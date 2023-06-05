9124e4c13805a38eb2e9cb7a04c2381e2941975f
Łukasz <109036862+luka443@users.noreply.github.com>
2023-06-06 00:44:38 +0200

    Add files via upload

--- markerPlacement.js
+++ markerPlacement.js
@@ -1,107 +1,102 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var element = document.getElementById("mapa");
+    var marker = document.getElementById("marker");
   
-            //jak sie srolluje( w wersji na kompa to sie cos psuje)
-            //w wersji na tel tw sumie tak samo ale tam jest jeszcze gorzej tak naprawde(bo jest wiecej scrollowania xd)
+    var isDragging = false;
+    var offsetX = 0;
+    var offsetY = 0;
+    var scrollX = 0;
+    var scrollY = 0;
+  
+    var buttonClicked = false;
+  
+    $(document).ready(function() {
+      $('#button').click(function(event) {
+        event.preventDefault();
+        buttonClicked = true;
+      });
+    });
+  
+    marker.addEventListener("mousedown", startDragging);
+    marker.addEventListener("touchstart", startDragging);
+  
+    document.addEventListener("mousemove", moveMarker);
+    document.addEventListener("touchmove", moveMarker, { passive: false });
+  
+    document.addEventListener("mouseup", stopDragging);
+    document.addEventListener("touchend", stopDragging);
+  
+    element.addEventListener("click", placeMarker);
+    element.addEventListener("touchstart", placeMarker);
+  
+    function startDragging(event) {
+      isDragging = true;
+      offsetX = getPageX(event) - marker.getBoundingClientRect().left;
+      offsetY = getPageY(event) - marker.getBoundingClientRect().top;
+    }
+  
 
-            var element = document.getElementById("mapa");
-            var marker = document.getElementById("marker");
-
-            var isDragging = false;
-            var offsetX = 0;
-            var offsetY = 0;
-            var scrollX = 0;
-            var scrollY = 0;
-
-
-            var buttonClicked = false;
-    
-            $(document).ready(function() {
-             
-              $('#button').click(function(event) {
-                event.preventDefault();
-                buttonClicked = true;
-                
-              });
-          
-            });
-            //powinno dzialac jak przesuwasz palcem po telefonie
-
-            marker.addEventListener("mousedown", startDragging);
-            marker.addEventListener("touchstart", startDragging);
-            
-
-
-            document.addEventListener("mousemove", moveMarker);
-            document.addEventListener("touchmove", moveMarker, { passive: false });
-
-
-
-
-
-            document.addEventListener("mouseup", stopDragging);
-            document.addEventListener("touchend", stopDragging);
-
-            element.addEventListener("click", placeMarker);
-            element.addEventListener("touchstart", placeMarker);
-
-
-            function startDragging(event) {
-                isDragging = true;
-                offsetX = getClientX(event) - marker.getBoundingClientRect().left;
-                offsetY = getClientY(event) - marker.getBoundingClientRect().top;
-            }
-
-            
-            function moveMarker(event) {
-              if (!buttonClicked && isDragging) {
-                console.log(buttonClicked);
-                event.preventDefault();
-                var x = getClientX(event) - offsetX;
-                var y = getClientY(event) - offsetY;
-            
-                marker.style.left = x + "px";
-                marker.style.top = y + "px";
-              }
-            }
-           
-
-            function stopDragging() {
-                isDragging = false;
-            }
-
-            function placeMarker(event) {
-                if(!buttonClicked){
-                    var x = getClientX(event);
-                    var y = getClientY(event);
-
-                    marker.style.left = x + "px";
-                    marker.style.top = y + "px";
-                    marker.style.display = "block";
-
-                    console.log("Coordinates: " + x + ", " + y);
-            }
-            }
-
-            function getClientX(event) {
-                if (event.touches && event.touches.length) {
-                    return event.touches[0].clientX;
-                } else {
-                    return event.clientX;
-                }
-            }
-
-            function getClientY(event) {
-                if (event.touches && event.touches.length) {
-                    return event.touches[0].clientY;
-                } else {
-                    return event.clientY;
-                }
-            }
-
-            element.addEventListener("scroll", function() {
-                scrollX = element.scrollLeft;
-                scrollY = element.scrollTop;
-            });
-        
-        
-        });
+  
+    function stopDragging() {
+      isDragging = false;
+    }
+  
+    function moveMarker(event) {
+        if (!buttonClicked && isDragging) {
+          event.preventDefault();
+          var containerRect = element.getBoundingClientRect();
+          var x = getPageX(event) - containerRect.left - offsetX + scrollX;
+          var y = getPageY(event) - containerRect.top - offsetY + scrollY;
+      
+          marker.style.left = x + "px";
+          marker.style.top = y + "px";
+        }
+      }
+      
+      function placeMarker(event) {
+        if (!buttonClicked) {
+          var containerRect = element.getBoundingClientRect();
+          var x = getPageX(event) - containerRect.left + scrollX;
+          var y = getPageY(event) + scrollY;
+      
+          marker.style.left = x + "px";
+          marker.style.top = y + "px";
+          marker.style.display = "block";
+      
+          console.log("Coordinates: " + x + ", " + y);
+        }
+    }
+  
+    function getPageX(event) {
+        if (event.touches && event.touches.length) {
+          return event.touches[0].pageX;
+        } else {
+          return event.pageX;
+        }
+      }
+      
+      function getPageY(event) {
+        if (event.touches && event.touches.length) {
+          return event.touches[0].pageY;
+        } else {
+          return event.pageY;
+        }
+      }
+      
+  
+    element.addEventListener("scroll", function() {
+      scrollX = element.scrollLeft;
+      scrollY = element.scrollTop;
+  
+      // Get the marker's position relative to the container
+      var markerRect = marker.getBoundingClientRect();
+      var containerRect = element.getBoundingClientRect();
+      var markerX = markerRect.left - containerRect.left;
+      var markerY = markerRect.top - containerRect.top;
+  
+      // Update the marker's position after scrolling
+      marker.style.left = markerX + scrollX + "px";
+      marker.style.top = markerY + scrollY + "px";
+    });
+  });
+  
