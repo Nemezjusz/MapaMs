10dcd253b40a10ce8c1a5e804213c71e6d803894
Szymon Sadowski <50834734+Nemezjusz@users.noreply.github.com>
2023-12-02 17:34:37 +0100

    Update helpers.inc.php

--- helpers.inc.php
+++ helpers.inc.php
@@ -9,17 +9,12 @@ function get_domain(): string {
 }
 
 class DB {
-    private static $dbh = null; // tutaj będziemy przechowywać obiekt PDO
+    private static $dbh = null; 
 
     public static function getInstance(): PDO {
 
-        // Jeśli w zmiennej statycznej self::$dbh nie mamy jeszcze utworzonego połączenia do bazy danych to je tworzymy.
         if (!self::$dbh) {
-            /*
-             * Instrukcja try-catch występuje w wielu językach programowania. Służy ona do przechwycenia wyjątku (błędu)
-             * rzuconego przez kod znajdujący się w środku bloku "try" oraz odpowiednią reakcję na rzucony wyjątek
-             * w bloku "catch".
-             */
+           
             try {
 
                 self::$dbh = new PDO(
@@ -32,38 +27,23 @@ class DB {
                 exit("Cannot connect to the database: " . $e->getMessage());
             }
         }
-        // Zwracamy obiekt PDO, reprezentujący połączenie z bazą danych.
         return self::$dbh;
     }
 }
 
 
 class TwigHelper {
-    private static $twig = null; // tutaj będziemy przechowywać obiekt Twiga
-    private static $msg = []; // tutaj będziemy przechowywać komunikaty do wyświetlenia w szablonie base.html
+    private static $twig = null; 
+    private static $msg = []; 
 
     public static function getInstance(): \Twig\Environment {
-        // Jeśli w zmiennej statycznej self::$twig nie mamy jeszcze utworzonego obiektu Twiga to go tworzymy.
         if (!self::$twig) {
-            /*
-             * Inicjalizujemy obiekt biblioteki Twig zgodnie z dokumentacją ze strony:
-             * https://twig.symfony.com/doc/3.x/api.html
-             * Obiekt przypisujemy do zmiennej statycznej self::$twig. Dzięki temu w dowolnym miejscu kodu możemy
-             * uzyskać do niego dostęp za pomocą TwigSingleton::getInstance().
-             */
+          
             $twig_loader = new \Twig\Loader\FilesystemLoader('templates');
             self::$twig = new \Twig\Environment($twig_loader);
         }
-        // Zwracamy obiekt Twiga
         return self::$twig;
     }
-
-    /*
-     * Klasa TwigHelper dostarczy nam jeszcze jednej przydatnej metody. W tablicy self::$msg trzymane będą
-     * komunikaty, które będzie wyświetlać szablon base.html nad treścią podstrony. Możesz dodawać dowolne komunikaty
-     * za pomocą TwigHelper::getInstance()->addMsg($text, $type). Przykłady użycia znajdziesz w pliku main.php.
-     * Jako typ możesz przekazać ciąg tekstowy: success, info, error. Typ decyduje o kolorze tła komunikatu.
-     */
     public static function addMsg(string $text, string $type): void {
         self::$msg[] = [
             'text' => $text,
@@ -71,17 +51,12 @@ class TwigHelper {
         ];
     }
 
-    // Metoda zwraca całą tablicę z komunikatami - będzie jej używać szablon base.html.
     public static function getMsg(): array {
         return self::$msg;
     }
 }
 
 
-
-
-// Konfigurujemy Twiga tak, aby w każdym szablonie była dostępna stała CONFIG, funkcja get_domain(),
-// oraz metoda TwigHelper::getMsg (pod nazwą get_msg), bez konieczności ręcznego przekazywania ich podczas renderowania.
 TwigHelper::getInstance()->addGlobal('CONFIG', CONFIG);
 TwigHelper::getInstance()->addFunction(new \Twig\TwigFunction('get_domain', 'get_domain'));
 TwigHelper::getInstance()->addFunction(new \Twig\TwigFunction('get_msg', 'TwigHelper::getMsg'));
